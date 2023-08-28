7afbafebb5df1c605ae189a2423859623a44589a
vrundpadariya <133338321+vrundpadariya@users.noreply.github.com>
2023-08-28 13:00:04 +0530

    Add files via upload

---
+++ emclass.dart
@@ -0,0 +1,16 @@
+class employee{
+
+int id;
+String name;
+String Designation;
+int salary;
+int Experience;
+String companyname;
+int age;
+String Qualification;
+String gender;
+String moblino;
+
+ employee({required this.id, required this.name,required this.Designation,required this.salary,required this.Experience,required this.companyname,required this.age,required this.Qualification,required this.gender,required this.moblino,})
+
+}
